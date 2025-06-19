13db67a3cfd09c7d2f9114d4f98794f6ea096113
Dharma Yudistira <dharmayudistira2000@gmail.com>
2025-06-20 01:16:02 +0700

    fix: import base url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,23 +1,21 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-
   return [
     {
-      url: `${baseUrl}/`,
+      url: `https://www.dharma-yudistira.com/`,
       lastModified: new Date(),
       priority: 1,
       changeFrequency: "monthly",
     },
     {
-      url: `${baseUrl}/blogs`,
+      url: `https://www.dharma-yudistira.com/blogs`,
       lastModified: new Date(),
       priority: 0.9,
       changeFrequency: "monthly",
     },
     {
-      url: `${baseUrl}/playgrounds`,
+      url: `https://www.dharma-yudistira.com/playgrounds`,
       lastModified: new Date(),
       priority: 0.8,
       changeFrequency: "monthly",
