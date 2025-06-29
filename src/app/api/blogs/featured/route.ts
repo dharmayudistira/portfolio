45399684f7ba058bc141710464a69fb03e0f329f
Dharma Yudistira <dharmayudistira2000@gmail.com>
2025-06-29 22:32:19 +0700

    fix: api linting

--- src/app/api/blogs/featured/route.ts
+++ src/app/api/blogs/featured/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { fetchFeaturedPosts } from "@/lib/hashnode";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const featuredPosts = await fetchFeaturedPosts();
 
